bd500cebba74a5732291f7b063ed6a4e2c233a7a
Kevin Gage <dudeman02379@gmail.com>
2015-08-11 13:53:27 -0400

    edited display so each item is a div and span.  Div uses ticketDiv css class which isnt setup yet

--- Client/scripts/main.js
+++ Client/scripts/main.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
     $.getJSON( "/TicketsWcf/TicketsService.svc/GetOpenTickets", function (data) {
+		/*
 		$.each (data, function (index, ticketObject) {
 			$.each (ticketObject, function (key, val){
 				$("#wrapper").append("<p>" + key + " " + val + "</p>");
 			}) 
+		});
+		*/
+		$.each (data, function (index, ticketObject) {
+			var ticketDiv = document.createElement("div");
+			$(ticketDiv).addClass("ticketdiv");
+			
+			var ticketDivText = document.createElement("span");
+			ticketDivText.innerHTML = ticketObject.Description;
+			$(ticketDiv).append(ticketDivText);
+			
+			$("#wrapper").append(ticketDiv);
 		});
 	});
 });
