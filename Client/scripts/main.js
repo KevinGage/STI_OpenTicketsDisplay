25f06eb26d0e6f7dafdbcf43b5b7eda2bdb923bc
Kevin Gage <dudeman02379@gmail.com>
2015-08-11 14:02:53 -0400

    Added client and assigned to display.  Createed subdivs with classes

--- Client/scripts/main.js
+++ Client/scripts/main.js
@@ -10,10 +10,31 @@ $( document ).ready(function() {
 		$.each (data, function (index, ticketObject) {
 			var ticketDiv = document.createElement("div");
 			$(ticketDiv).addClass("ticketdiv");
-			
-			var ticketDivText = document.createElement("span");
-			ticketDivText.innerHTML = ticketObject.Description;
-			$(ticketDiv).append(ticketDivText);
+		
+			var clientDiv = document.createElement("div");
+			$(clientDiv).addClass("clientdiv");
+
+			var clientDivText = document.createElement("span");
+			clientDivText.innerHTML = ticketObject.ClientName;
+			$(clientDiv).append(clientDivText);
+		
+			var descriptionDiv = document.createElement("div");
+			$(descriptionDiv).addClass("deescriptiondiv");
+
+			var descriptionDivText = document.createElement("span");
+			descriptionDivText.innerHTML = ticketObject.Description;
+			$(descriptionDiv).append(descriptionDivText);
+
+			var assignDiv = document.createElement("div");
+			$(assignDiv).addClass("assigndiv");
+
+			var assignDivText = document.createElement("span");
+			assignDivText.innerHTML = ticketObject.AssignTech;
+			$(assignDiv).append(assignDivText);
+
+			$(ticketDiv).append(clientDiv);
+			$(ticketDiv).append(descriptionDiv);
+			$(ticketDiv).append(assignDiv);
 			
 			$("#wrapper").append(ticketDiv);
 		});
