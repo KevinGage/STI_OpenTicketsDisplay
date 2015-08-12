d2be385c700ebd4c408ddd73736033c122f47922
KevinGage <gageke@yahoo.com>
2015-08-12 10:08:12 -0400

    created seperate functions for create, update, and delete divs.  Created setInterval framwork for a new check every 30 seconds

--- Client/scripts/main.js
+++ Client/scripts/main.js
@@ -1,36 +1,68 @@
+var oldTicketArray = [];
+var newTicketArray = [];
+
+
 $( document ).ready(function() {
     $.getJSON( "/TicketsWcf/TicketsService.svc/GetOpenTickets", function (data) {
+		oldTicketArray = data;
 		$.each (data, function (index, ticketObject) {
-			var ticketDiv = document.createElement("div");
-			$(ticketDiv).addClass("ticketdiv");
-		
-			var clientDiv = document.createElement("div");
-			$(clientDiv).addClass("clientdiv");
-
-			var clientDivText = document.createElement("span");
-			clientDivText.innerHTML = ticketObject.ClientName;
-			$(clientDiv).append(clientDivText);
-		
-			var descriptionDiv = document.createElement("div");
-			$(descriptionDiv).addClass("deescriptiondiv");
-
-			var descriptionDivText = document.createElement("span");
-			descriptionDivText.innerHTML = ticketObject.Description;
-			$(descriptionDiv).append(descriptionDivText);
-
-			var assignDiv = document.createElement("div");
-			$(assignDiv).addClass("assigndiv");
-
-			var assignDivText = document.createElement("span");
-			assignDivText.innerHTML = ticketObject.AssignTech;
-			$(assignDiv).append(assignDivText);
-
-			$(ticketDiv).append(clientDiv);
-			$(ticketDiv).append(descriptionDiv);
-			$(ticketDiv).append(assignDiv);
-			
-			$("#wrapper").append(ticketDiv);
-			
+			createDiv(ticketObject)	
 		});
 	});
+	
+	setInterval(function() {
+		$.getJSON( "/TicketsWcf/TicketsService.svc/GetOpenTickets", function (data) {
+			newTicketArray = data;
+			
+			for (var i = 0; i < oldTicketArray.length; i++){
+				//check for ticket numbers in new array.  Anything found compare the rest of the ticket info and update div if necissary. anything missing close
+				
+			}
+			for (var i = 0; i < newTicketArray.length; i++){
+				//check for ticket numbers in old array.  Anything missing open
+			}
+		});
+	}, 30000);
 });
+
+function updateDiv(ticketNum){
+	
+}
+
+function deleteDiv(ticketNun){
+	
+}
+
+function createDiv(ticket){
+	var ticketDiv = document.createElement("div");
+	$(ticketDiv).addClass("ticketdiv");
+	$(ticketDiv).data(ticket);
+	
+	var clientDiv = document.createElement("div");
+	$(clientDiv).addClass("clientdiv");
+
+	var clientDivText = document.createElement("span");
+	clientDivText.innerHTML = ticket.ClientName;
+	$(clientDiv).append(clientDivText);
+
+	var descriptionDiv = document.createElement("div");
+	$(descriptionDiv).addClass("deescriptiondiv");
+
+	var descriptionDivText = document.createElement("span");
+	descriptionDivText.innerHTML = ticket.Description;
+	$(descriptionDiv).append(descriptionDivText);
+
+	var assignDiv = document.createElement("div");
+	$(assignDiv).addClass("assigndiv");
+
+	var assignDivText = document.createElement("span");
+	assignDivText.innerHTML = ticket.AssignTech;
+	$(assignDiv).append(assignDivText);
+
+	$(ticketDiv).append(clientDiv);
+	$(ticketDiv).append(descriptionDiv);
+	$(ticketDiv).append(assignDiv);
+	
+	$("#wrapper").append(ticketDiv);
+}
+
